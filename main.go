0bed8538479246a1a4e5fa63032b302e22694662
Evan Ercolano <evanercolano@gmail.com>
2018-12-12 22:32:18 -0500

    Implemented the gorilla mux router

--- main.go
+++ main.go
@@ -6,21 +6,23 @@ package main
 import (
 	"fmt"      // useful for printing data to diff places
 	"net/http" // used for web server or making web requests
+
+	"github.com/gorilla/mux"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	if r.URL.Path == "/contact" {
-		fmt.Fprint(w, "To get in touch, please send an email to <a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a>.")
-	} else if r.URL.Path == "/" {
-		fmt.Fprint(w, "<h1> Welcome to my awesome website </h1>")
-	} else {
-		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprint(w, "404 not found")
-	}
+	fmt.Fprint(w, "<h1> Welcome to my awesome website </h1>")
+}
+
+func contact(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "To get in touch, please send an email to <a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a>.")
 }
 
 func main() {
-	http.HandleFunc("/", handlerFunc)
-	http.ListenAndServe(":3000", nil)
+	r := mux.NewRouter()
+	r.HandleFunc("/", home)
+	r.HandleFunc("/contact", contact)
+	http.ListenAndServe(":3000", r)
 }
