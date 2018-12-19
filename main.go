06ea168e3e654ba6ebd4359bb414d57d21291bb6
Evan Ercolano <evanercolano@gmail.com>
2018-12-19 11:12:47 -0500

    Use bootstrap

--- main.go
+++ main.go
@@ -4,8 +4,6 @@ package main
 // requires a main function
 
 import (
-	// useful for printing data to diff places
-
 	"net/http" // used for web server or making web requests
 
 	"lenslocked.com/views"
@@ -20,7 +18,7 @@ var (
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	err := homeView.Template.Execute(w, nil)
+	err := homeView.Template.ExecuteTemplate(w, homeView.Layout, nil)
 	if err != nil {
 		panic(err)
 	}
@@ -28,15 +26,15 @@ func home(w http.ResponseWriter, r *http.Request) {
 
 func contact(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	err := contactView.Template.Execute(w, nil)
+	err := contactView.Template.ExecuteTemplate(w, contactView.Layout, nil)
 	if err != nil {
 		panic(err)
 	}
 }
 
 func main() {
-	homeView = views.NewView("views/home.gohtml")
-	contactView = views.NewView("views/contact.gohtml")
+	homeView = views.NewView("bootstrap", "views/home.gohtml")
+	contactView = views.NewView("bootstrap", "views/contact.gohtml")
 
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
