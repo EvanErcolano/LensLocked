bc81ffb9e8c56a3ca745d0fc35e025b4c445734b
Evan Ercolano <evanercolano@gmail.com>
2019-01-30 18:57:20 -0500

    Created the gallery model

---
+++ models/galleries.go
@@ -0,0 +1,13 @@
+package models
+
+import (
+	"github.com/jinzhu/gorm"
+)
+
+// Gallery is our image container resource that visitors
+// view
+type Gallery struct {
+	gorm.Model
+	UserID uint   `gorm:"not_null;index"`
+	Title  string `gorm:"not_null"`
+}
