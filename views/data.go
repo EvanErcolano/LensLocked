f0ea2c745d4eca8ebf03798c167d3cf47b3f5956
Evan Ercolano <evanercolano@gmail.com>
2019-02-02 14:53:43 -0500

    Add logging for errors that aren't public for debugging

--- views/data.go
+++ views/data.go
@@ -1,5 +1,7 @@
 package views
 
+import "log"
+
 const (
 	AlertLvlError   = "danger"
 	AlertLvlWarning = "warning"
@@ -33,6 +35,7 @@ func (d *Data) SetAlert(err error) {
 			Message: pErr.Public(),
 		}
 	} else {
+		log.Println(err.Error())
 		d.Alert = &Alert{
 			Level:   AlertLvlError,
 			Message: AlertMsgGeneric,
